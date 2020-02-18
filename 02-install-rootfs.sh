87f27085cc1c883309e32f0d494dbbdb618ec6c4
Nikhil Jha <hi@nikhiljha.com>
2020-02-18 14:54:17 -0800

    introduce race condition

--- 02-install-rootfs.sh
+++ 02-install-rootfs.sh
@@ -53,6 +53,8 @@ then
 
     infecho "Mounting SD Card rootfs..."
     partprobe -s $PP_SD_DEVICE
+    blockdev --rereadpt $PP_SD_DEVICE
+    sleep 1 # Sometimes it lags.
     mount $PP_PARTB rootfs
 
     infecho "Copying files..."
