1bd118ac7487e3d88438134483cc964b963143c9
Nikhil Jha <hi@nikhiljha.com>
2020-03-03 15:40:39 -0800

    add image creation to script

--- all.sh
+++ all.sh
@@ -4,6 +4,7 @@ set -e
 echo "This script will NOT download the files."
 echo "Do it yourself: sh 00-download-files.sh"
 bash 00-selftest.sh
+bash 00-create-empty-image.sh
 bash 01-partition-drive.sh
 bash 02-install-rootfs.sh
 bash 03-install-kernel.sh
